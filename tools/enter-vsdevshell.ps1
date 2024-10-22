23de97b5b21e50ff80515c9180caf5b47dff1ff4
Jesper Duemose Nielsen <jesperduemose@gmail.com>
2024-10-22 10:53:41 +0200

    try command prompt

--- tools/enter-vsdevshell.ps1
+++ tools/enter-vsdevshell.ps1
@@ -1,4 +1,3 @@
-echo HELLO
 $vs = & "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -latest -format json | ConvertFrom-Json
 Import-Module "$($vs.InstallationPath)\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Enter-VsDevShell $vs.InstanceId -DevCmdArguments '-arch=x64'
